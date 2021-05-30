26c649bfa130102b5614e9ecfc2162000af85c47
Mateuslpds <mateusyasmim2014@gmail.com>
2021-05-29 21:08:28 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,89 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+snake[0] = {
+	x: 8 * box,
+	y: 8 * box
+}
+let direction = "right";
+let comida = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
+}
+var pontos = 0;
+
+
+function criarBG(){
+	context.fillStyle = "#9bc304";
+	context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+	for (var i = 0; i < snake.length; i++) {
+		context.fillStyle = "#080d05";
+		context.fillRect(snake[i].x, snake[i].y, box, box);
+	}
+}
+
+function criarComida(){
+	context.fillStyle = "#21251e";
+	context.fillRect(comida.x, comida.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+	if (event.keyCode == 37 && direction != "right") direction = "left";
+	if (event.keyCode == 38 && direction != "down") direction = "up";
+	if (event.keyCode == 39 && direction != "left") direction = "right";
+	if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+	if (snake[0].x > 15 * box) snake[0].x = 0;
+	if (snake[0].x < 0) snake[0].x = 16 * box;
+	if (snake[0].y > 15 * box) snake[0].y = 0;
+	if (snake[0].y < 0) snake[0].y = 16 * box;
+
+	for(i = 1; i < snake.length; i++){
+		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+			clearInterval(jogo);
+			alert('Game Over');
+		}
+	}
+
+	criarBG();
+	criarCobrinha();
+	criarComida();
+
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if (direction == "right") snakeX += box;
+	if (direction == "left") snakeX -= box;
+	if (direction == "up") snakeY -= box;
+	if (direction == "down") snakeY += box;
+
+	if (snakeX != comida.x || snakeY != comida.y){
+		snake.pop();
+
+	}else{
+		comida.x = Math.floor(Math.random() * 15 + 1) * box;
+		comida.y = Math.floor(Math.random() * 15 + 1) * box;
+		pontos++;
+		console.log(pontos);
+		document.getElementById('output').innerHTML = pontos;
+	}
+
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	}
+
+	snake.unshift(newHead);
+}
+
+document.getElementById('output').innerHTML = pontos;
+
+let jogo = setInterval(iniciarJogo, 100);
